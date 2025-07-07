696e8bd86566732585e8c9c0f98db53de00b28bc
Leandro <nunes.pessoal@gmail.com>
2025-07-07 19:33:15 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using MongoApiExample.Models;
+using MongoApiExample.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDBSettings>(
+    builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDBService>();
+
+builder.Services.AddControllers(); // <-- Necessário!
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers(); // <-- Necessário!
+
+app.Run();
---
+++ Services/MongoDBService.cs
@@ -0,0 +1,45 @@
+using MongoApiExample.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoApiExample.Services
+{
+    public class MongoDBService
+    {
+        private readonly IMongoCollection<Playlist> _playlistsCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _playlistsCollection = database.GetCollection<Playlist>(mongoDBSettings.Value.CollectionName);
+        }
+
+        public async Task CreateAsync(Playlist playlist)
+        {
+            await _playlistsCollection.InsertOneAsync(playlist);
+            return;
+        }
+
+        public async Task<List<Playlist>> GetAsync()
+        {
+            return await _playlistsCollection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task AddToPlaylistAsync(string id, string movieId)
+        {
+            FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Id", id);
+            UpdateDefinition<Playlist> update = Builders<Playlist>.Update.AddToSet<string>("movieId", movieId);
+            await _playlistsCollection.UpdateOneAsync(filter, update);
+            return;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Id", id);
+            await _playlistsCollection.DeleteOneAsync(filter);
+            return;
+        }
+    }
+}
